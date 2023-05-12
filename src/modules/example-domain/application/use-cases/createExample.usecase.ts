c04738a3a094c4af176b621108ab96a9a921772e
Lino Aboy <linodeveloper85@gmail.com>
2023-05-12 13:28:07 -0400

    feat: the example use case now uses transactions

--- src/modules/example-domain/application/use-cases/createExample.usecase.ts
+++ src/modules/example-domain/application/use-cases/createExample.usecase.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@nestjs/common';
+import { mongooseTransactionSessionCreator } from
+    'src/modules/shared/infrastructure/persistence/mongooseTransactionSessionCreator';
 import { ExampleRepository } from '../../infrastructure/adapters/persistence/example.repository';
 import { CreateExampleDto } from '../dto/createExampleDto';
 import { RepositoryPortCreateInterface } from '../ports/repository-port';
@@ -12,10 +14,21 @@ export class CreateExample implements RestAdapterPortCreateExampleInterface {
     }
 
     async handler(example: CreateExampleDto): Promise<void> {
+        const session = await mongooseTransactionSessionCreator();
+
+        (this.exampleRepository as ExampleRepository).setTransactionSession(session);
+
         try {
             await this.exampleRepository.create(example);
+
+            await session.commitTransaction();
         } catch (error) {
+            await session.abortTransaction();
+
             throw new Error(error.message);
+        }
+        finally {
+            session.endSession();
         }
     }
 }
